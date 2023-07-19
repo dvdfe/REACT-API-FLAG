a6ff1de9a9c7fbb305e3f3f951799fee520ae342
DVDFE <davidfe.pro@gmail.com>
2023-07-19 16:12:54 +0200

    ajout axios.post + possibilité d'ajouter des posts et d'incrémenter la base de données

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -3,11 +3,14 @@ import Logo from "./Logo";
 import Navigation from "./Navigation";
 import { useState } from "react";
 import axios from "axios";
+import Article from "./Article";
 
 const Blog = () => {
   const [content, setContent] = useState("");
   const [error, setError] = useState(false);
-  const [blogData, setBlogData] = useState([])
+  const [blogData, setBlogData] = useState([]);
+  const [author, setAuthor] = useState("");
+  // const [postData, setPostData] = useState([]);
 
   const getData = () => {
     axios
@@ -15,8 +18,18 @@ const Blog = () => {
       .then((res) => setBlogData(res.data));
   };
 
+  // const postData = () => {
+  //   axios
+  //     .post("http://localhost:3004/articles")
+  //     .then((res) => setPostData(res.data));
+  // };
+
+  // useEffect(() => {
+  //   postData();
+  // }, []);
+
   useEffect(() => {
-      getData()
+    getData();
   }, []);
 
   const handleSubmit = (e) => {
@@ -24,7 +37,15 @@ const Blog = () => {
     if (content.length < 140) {
       setError(true);
     } else {
+      axios.post("http://localhost:3004/articles", {
+        author,
+        content,
+        date: Date.now(),
+      });
       setError(false);
+      setAuthor("");
+      setContent("");
+      getData();
     }
   };
   return (
@@ -33,15 +54,28 @@ const Blog = () => {
       <Navigation />
       <h1>Blog</h1>
       <form onSubmit={(e) => handleSubmit(e)}>
-        <input type="text" placeholder="Nom" />
+        <input
+          type="text"
+          placeholder="Nom"
+          onChange={(e) => setAuthor(e.target.value)}
+          value={author}
+        />
         <textarea
           style={{ border: error ? "1px solid red" : "1px solid #61dafb" }}
           placeholder="Message"
           onChange={(e) => setContent(e.target.value)}
+          value={content}
         ></textarea>
         {error && <p>Veuillez écrire un minimum de 140 caractères</p>}
         <input type="submit" value="Envoyer" />
       </form>
+      <ul>
+        {blogData
+          .sort((a, b) => b.date - a.date)
+          .map((article) => (
+            <Article key={article.id} article={article} />
+          ))}
+      </ul>
     </div>
   );
 };
